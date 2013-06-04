ecd70bb0bba866bc66a67a636629413cc108c93e
Ryan Smith <r@32k.io>
2013-06-04 21:11:46 +0000

    simplify memory allocation

--- main.c
+++ main.c
@@ -22,8 +22,6 @@ void handle_req(void *);
 void
 taskmain(int argc , char **argv)
 {
-	setlinebuf(stdout);
-	setlinebuf(stderr);
 	DB_ENV *env;
 	DB *db;
 
@@ -50,7 +48,6 @@ taskmain(int argc , char **argv)
 	}
 	fdnoblock(fd);
 	while((cfd = netaccept(fd, remote, &rport)) >= 0) {
-		fprintf(stderr, "Clien conn established.\n");
 		conn *c = malloc(sizeof(conn));
 		c->db = db;
 		c->env = env;
@@ -92,7 +89,6 @@ readnbytes(int fd, int nbytes, char *buff)
 			return n;
 		}
 		nread += n;
-		fprintf(stderr, "nbytes=%d nread=%d n=%d buff=%s\n", nbytes, nread, n, (char *)buff);
 	}
 	return nread;
 }
@@ -104,91 +100,84 @@ handle_req(void *v)
 	conn *c;
 	c = (conn *)v;
 
-	char lens[16];
-	memset(&lens, 0, sizeof lens);
+	char lens[4];
+	memset(&lens, 0, 4);
 	int n;
 	n = readuntil(c->fd, ' ', lens);
 	int len;
 	len = atoi(lens);
 
-	char buff[2048];
-	memset(buff, 0, sizeof buff);
+	char buff[len + 1];
+	memset(buff, 0, len + 1);
 	readnbytes(c->fd, len, buff);
 
-	char body[2048];
-	memset(body, 0, sizeof body);
+	char body[len - 2];
 	strncpy(body, buff+2, len - 2);
 
-	char resp[2048];
-	memset(resp, 0, sizeof resp);
-
+	char *resp;
 	if (buff[0] == 'p') {
-		if ((ret = put(c->db, body, resp)) != 0) {
-			strcpy(resp, "e\n");
+		if ((ret = put(c->db, body, &resp)) <= 0) {
+			ret = build_error("e unable to put", &resp);
 			fprintf(stderr, "error=%s\n", db_strerror(ret));
 		}
 	} else if (buff[0] == 'g') {
-		if ((ret = get(c->db, body, resp)) != 0) {
-			strcpy(resp, "e\n");
+		if ((ret = get(c->db, body, &resp)) <= 0) {
+			ret = build_error("e unable to put", &resp);
 			fprintf(stderr, "error=%s\n", db_strerror(ret));
 		}
 	} 
-	fprintf(stderr, "body=%s resp=%s\n", body, resp);
-	fdwrite(c->fd, resp, strlen(resp));
-
-	memset(resp, 0, sizeof resp);
-	memset(body, 0, sizeof body);
-	memset(buff, 0, sizeof buff);
-
+	fdwrite(c->fd, resp, ret);
 	shutdown(c->fd);
 	close(c->fd);
 }
 
 int
-put(DB *db, char *body, char *resp)
+build_error(char *msg, char **resp)
 {
-	char id[36];
-	char *payload;
-	strncpy(id, body, 36);
-	payload = malloc(strlen(body) - 35);
-	strncpy(payload, body+37, strlen(body) - 35);
-
-	DBT k, v;
-	memset(&k, 0, sizeof(DBT));
-	memset(&v, 0, sizeof(DBT));
-	k.data = id;
-	k.size = strlen(id);
-	v.data = payload;
-	v.size = strlen(payload);
-
-	int ret;
-	ret = db->put(db, NULL, &k, &v, 0);
-	if (ret != 0) {
-		return ret;
-	}
-
-	strcpy(resp, strcat(id, "\n"));
-	return 0;
+	*resp = malloc(strlen(msg));
+	strcpy(*resp, msg);
+	return strlen(msg);
 }
 
 int
-get(DB *db, char *body, char *resp)
+put(DB *db, char *body, char **resp)
 {
-	char id[36];
-	strncpy(id, body, 36);
-
-	DBT k, v;
-	memset(&k, 0, sizeof(DBT));
-	memset(&v, 0, sizeof(DBT));
-	k.data = id;
-	k.size = strlen(id);
-	
 	int ret;
+	DBT k, v;
+
+	memset(&k, 0, sizeof(k));
+	memset(&v, 0, sizeof(v));
+
+	k.size = 36;
+	k.data = body; 
+	v.size = strlen(body) - 37;
+	v.data = body + 37;
+
+	if ((ret = db->put(db, NULL, &k, &v, 0)) != 0) {
+		return ret;
+	}
+
+	*resp = k.data;
+	return k.size;
+}
+
+int
+get(DB *db, char *body, char **resp)
+{
+	int ret;
+	DBT k, v;
+	memset(&k, 0, sizeof(k));
+	memset(&v, 0, sizeof(v));
+
+	k.size = 36;
+	k.data = body;
+	
 	if ((ret = db->get(db, NULL, &k, &v, 0)) != 0) {
 		return ret;
 	}
-	memcpy(resp, (char *)v.data, v.size);
-	return 0;
+
+	*resp = v.data;
+	return v.size;
 }
 
 int 
