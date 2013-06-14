968a9cae3fc0d9bf271dbba631b3da9fd44dd2a9
Ryan Smith <r@32k.io>
2013-06-14 00:41:00 +0000

    top down

--- main.c
+++ main.c
@@ -11,86 +11,13 @@ enum
         STACK = 32768
 };
 
+//This struct makes it handy to pass state to
+//an async task. See taskmain and handle_req for usage.
 typedef struct {
 	DB_ENV *env;
 	DB *db;
 	int fd;
 } conn;
-
-void handle_req(void *);
-
-void
-taskmain(int argc , char **argv)
-{
-	DB_SITE *site;
-	DB_ENV *env;
-	DB *db;
-
-	int ret;
-	if ((ret = init_env(&env)) != 0) {
-		fprintf(stderr, "init_env error: %s\n", db_strerror(ret));
-		exit(1);
-	}
-	printf("at=env-initialized\n");
-
-	if ((ret = init_site(env, 1)) != 0) {
-		fprintf(stderr, "init_db_site error: %s\n", db_strerror(ret));
-		exit(1);
-	}
-	printf("at=rep-initialized\n");
-
-	if ((ret = init_db(&db, env)) != 0) {
-		fprintf(stderr, "init_db error: %s\n", db_strerror(ret));
-		exit(1);
-	}
-	printf("at=db-initialized\n");
-
-	//Use 3 threads for processing replication events.
-	if ((ret = env->repmgr_start(env, 3, DB_REP_ELECTION)) != 0) {
-		fprintf(stderr, "repmgr error: %s\n", db_strerror(ret));
-		exit(1);
-	}
-
-	int cfd, fd;
-	int rport;
-	char remote[16];
-
-	fd = netannounce(TCP, 0, 8000);
-	if (fd < 0) {
-		fprintf(stderr, "Unable to announce on port 8000.\n");
-		exit(1);
-	}
-	fdnoblock(fd);
-	while((cfd = netaccept(fd, remote, &rport)) >= 0) {
-		conn *c = malloc(sizeof(conn));
-		c->db = db;
-		c->env = env;
-		c->fd = cfd;
-		taskcreate(handle_req, (void *)c, STACK);
-	}
-	return;
-}
-
-//Read from fd until we find c.
-//Bytes up till c will be stored in buff.
-int
-readuntil(int fd, char c, char *buff)
-{
-	int n, found, nread;
-	nread = 0;
-	found = 0;
-
-	while(found == 0) {
-		if ((n = fdread(fd, buff + nread, 1)) < 1) {
-			return n;
-		}
-		if (buff[nread] == c) {
-			found = 1;
-		}
-		nread++;
-	}
-	return nread;
-}
 
 int
 readnbytes(int fd, int nbytes, char *buff)
@@ -105,6 +32,51 @@ readnbytes(int fd, int nbytes, char *buff)
 		nread += n;
 	}
 	return nread;
+}
+
+int
+put(DB *db, char *id, char *body, char **resp)
+{
+	int ret;
+	DBT k, v;
+
+	memset(&k, 0, sizeof(k));
+	memset(&v, 0, sizeof(v));
+
+	k.size = 36;
+	k.data = id; 
+	k.flags = DB_DBT_MALLOC;
+	v.size = strlen(body) + 1;
+	v.data = body;
+	v.flags = DB_DBT_MALLOC;
+
+	if ((ret = db->put(db, NULL, &k, &v, 0)) != 0) {
+		return ret;
+	}
+	*resp = v.data;
+	return v.size;
+}
+
+int
+get(DB *db, char *id, char **resp)
+{
+	int ret;
+	DBT k, v;
+	memset(&k, 0, sizeof(k));
+	memset(&v, 0, sizeof(v));
+
+	k.size = 36;
+	k.data = id;
+	k.flags = DB_DBT_MALLOC;
+
+	v.flags = DB_DBT_MALLOC;
+	
+	if ((ret = db->get(db, NULL, &k, &v, 0)) != 0) {
+		return ret;
+	}
+
+	*resp = v.data;
+	return v.size;
 }
 
 void
@@ -156,51 +128,6 @@ handle_req(void *v)
 	printf("disconnected\n");
 	shutdown(c->fd);
 	close(c->fd);
-}
-
-int
-put(DB *db, char *id, char *body, char **resp)
-{
-	int ret;
-	DBT k, v;
-
-	memset(&k, 0, sizeof(k));
-	memset(&v, 0, sizeof(v));
-
-	k.size = 36;
-	k.data = id; 
-	k.flags = DB_DBT_MALLOC;
-	v.size = strlen(body) + 1;
-	v.data = body;
-	v.flags = DB_DBT_MALLOC;
-
-	if ((ret = db->put(db, NULL, &k, &v, 0)) != 0) {
-		return ret;
-	}
-	*resp = v.data;
-	return v.size;
-}
-
-int
-get(DB *db, char *id, char **resp)
-{
-	int ret;
-	DBT k, v;
-	memset(&k, 0, sizeof(k));
-	memset(&v, 0, sizeof(v));
-
-	k.size = 36;
-	k.data = id;
-	k.flags = DB_DBT_MALLOC;
-
-	v.flags = DB_DBT_MALLOC;
-	
-	if ((ret = db->get(db, NULL, &k, &v, 0)) != 0) {
-		return ret;
-	}
-
-	*resp = v.data;
-	return v.size;
 }
 
 int 
@@ -294,4 +221,56 @@ init_site(DB_ENV *env, int creator)
 			return ret;
 		}
 	}
+}
+
+void
+taskmain(int argc , char **argv)
+{
+	DB_SITE *site;
+	DB_ENV *env;
+	DB *db;
+
+	int ret;
+	if ((ret = init_env(&env)) != 0) {
+		fprintf(stderr, "init_env error: %s\n", db_strerror(ret));
+		exit(1);
+	}
+	printf("at=env-initialized\n");
+
+	if ((ret = init_site(env, 1)) != 0) {
+		fprintf(stderr, "init_db_site error: %s\n", db_strerror(ret));
+		exit(1);
+	}
+	printf("at=rep-initialized\n");
+
+	if ((ret = init_db(&db, env)) != 0) {
+		fprintf(stderr, "init_db error: %s\n", db_strerror(ret));
+		exit(1);
+	}
+	printf("at=db-initialized\n");
+
+	//Use 3 threads for processing replication events.
+	if ((ret = env->repmgr_start(env, 3, DB_REP_ELECTION)) != 0) {
+		fprintf(stderr, "repmgr error: %s\n", db_strerror(ret));
+		exit(1);
+	}
+
+	int cfd, fd;
+	int rport;
+	char remote[16];
+
+	fd = netannounce(TCP, 0, 8000);
+	if (fd < 0) {
+		fprintf(stderr, "Unable to announce on port 8000.\n");
+		exit(1);
+	}
+	fdnoblock(fd);
+	while((cfd = netaccept(fd, remote, &rport)) >= 0) {
+		conn *c = malloc(sizeof(conn));
+		c->db = db;
+		c->env = env;
+		c->fd = cfd;
+		taskcreate(handle_req, (void *)c, STACK);
+	}
+	return;
 }
